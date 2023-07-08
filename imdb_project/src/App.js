6bacbd1eac1c0ba2879721fb3ad71a9be143c620
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2023-07-08 22:43:23 +0530

    code updated

---
+++ imdb_project/src/App.js
@@ -0,0 +1,12 @@
+import 'tailwindcss/tailwind.css';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-blue-600"> IMDB App</h1>
+    </div>
+  );
+}
+
+export default App;
