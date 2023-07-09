21850ae41bea3cabc36ef4f210d1d7552393c374
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2023-07-09 12:18:25 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import 'tailwindcss/tailwind.css';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-blue-600"> IMDB App</h1>
+    <div className='flex border space-x-8 items-center pl-4 py-5'>
+     <h3 className='text-blue-400'>Movies</h3>
+     <h3 className='text-blue-400'>Watchlist</h3>
+
     </div>
   );
 }
