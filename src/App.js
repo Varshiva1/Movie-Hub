f778d200698753beba9441127bbf2d5f544562dc
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2023-07-12 17:24:03 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Movies from './components/Movies';
+import WatchList from './components/WatchList';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
       <NavBar/>
+
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={
+          <>
       <Banner/>
       <Movies/>
+          </>
+        }>
+        </Route>
+
+   <Route path="/watchlist" element={<WatchList/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
