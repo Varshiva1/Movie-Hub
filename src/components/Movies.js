4ab8dd6f01971e2bb950693d1e733658bb431136
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2023-07-25 13:08:37 +0530

    code updated

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -12,20 +12,12 @@ function Movies() {
                  className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-5 md:h[40vh] md:w[180px] hover:scale-110 duration-300 relative flex '
                     style={{ backgroundImage: `url(${require('../images/card.webp')})` }}>
 
+                <div className='text-white font-bold text-centre w-full bg-gray-900 bg-opacity-60'>
+                  ALONE
                 </div>
-                
-
-                <div 
-                 className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-5 md:h[40vh] md:w[180px] hover:scale-110 duration-300 relative flex '
-                        style={{ backgroundImage: `url(${require('../images/card.webp')})` }}>
-
                 </div>
 
-                <div 
-                 className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-5 md:h[40vh] md:w[180px] hover:scale-110 duration-300 relative flex '
-                        style={{ backgroundImage: `url(${require('../images/card.webp')})` }}>
-
-                </div>
+               
 
              </div>
 
