4bd5a9538fa7a8055769a407a66ec2f15a3af1da
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2023-07-25 16:06:31 +0530

    api-caling

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,34 +1,43 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 
 import axios from 'axios';
+import { computeHeadingLevel } from '@testing-library/react';
 
 function Movies() {
 
+const [movies,setMovies]=useState([])
 
-axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=7af0a3ff53ab2ff0f3f224bb58d5b777`).then((res)=>{
-    console.log(res.data.results);
-})
+useEffect(()=>{
+    (function(){
 
+        axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=7af0a3ff53ab2ff0f3f224bb58d5b777`)
+        .then((res)=>{
+            setMovies(res.data.results);
+           
+        });
+    })()
+    
+    },[])
+console.log(movies);
     return (
         <div>
             <div className='text-2xl mb-8 font-bold text-center'>
                 Trending Movies
             </div>
-            <div className='flex'>
-
-                <div 
-                 className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-5 md:h[40vh] md:w[180px] hover:scale-110 duration-300 relative flex '
-                    style={{ backgroundImage: `url(${require('../images/card.webp')})` }}>
+            <div className='flex flex-wrap'>
+               
+                {movies.map((movie,index)=>
+                    (<div key={index}
+                className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-5 md:h[40vh] md:w[180px] hover:scale-110 duration-300 relative flex '
+                style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})` }}>
 
                 <div className='text-white font-bold text-centre w-full bg-gray-900 bg-opacity-60'>
-                  ALONE
+                  {movie.title}
                 </div>
-                </div>
-
-               
+                </div>)
+                    )}
 
              </div>
-
         </div>
  );
 }
