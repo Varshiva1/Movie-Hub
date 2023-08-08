ff40a7ce372f784d54af38e07436c708c1ff7a77
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2023-08-08 15:12:15 +0530

    add to  watchlist updated

---
+++ src/components/Card.jsx
@@ -0,0 +1,44 @@
+
+import star from '../images/star.png';
+import bookmark from '../images/bookmark.svg';
+import bookmark2 from '../images/bookmark2.svg';
+
+import { useState } from 'react';
+
+export default function Card({movie, addToWatchlist,removeFromWatchlist}) {
+const [toggle, setToggle] = useState(false);
+
+  return (
+    <div
+           
+            className='relative w-60 overflow-hidden rounded-xl border-4 hover:scale-105 duration-300 border-gray-800 text-white -inset-600 font-bold text-centre bg-opacity-20'
+          >
+
+
+        
+
+              <div className='drop-shadow-md hover:drop-shadow-xl absolute cursor-pointer hover:scale-110' onClick={() => {
+                setToggle(!toggle);
+                toggle ? 
+                 removeFromWatchlist(movie):addToWatchlist(movie)
+              }
+                }>
+                <img src={toggle ?  bookmark2:bookmark} className='w-[40px] mx-0.3' alt='img' />
+              </div>
+         
+            
+
+
+
+            <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt='img' />
+            <div className='flex gap-2 p-3 text-white'>
+              <img src={star} className='w-[20px] p-l-2' alt='img' />
+              {movie.vote_average.toFixed(1)}
+            </div>
+            <div className='flex p-3 text-white'>{movie.title}</div>
+          </div>
+  )
+}
+
+
+// box-shadow: 5px 5px 0px 0px rgba(109,40,217);
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import star from '../images/star.png';
 import axios from 'axios';
-import bookmark from '../images/bookmark (1).png';
 import Pagination from './Pagination';
 import WatchList from './WatchList';
+import Card from './Card';
 
 function Movies() {
   const [movies, setMovies] = useState([]);
@@ -23,19 +22,10 @@ function Movies() {
 
   const addToWatchlist = (movie) => {
 
-    let isPresent = watchList?.find((ele)=>ele.id === movie.id);
+    let isPresent = watchList?.find((ele) => ele.id === movie.id);
 
-    if(!isPresent)
-    setWatchList([...watchList, movie]);
-
-  
-
-
-    // if(watchList.length === 0 ) {
-    // }
-    // else{
-    // }
-    // if(watchList.filter((ele)=>ele.id === movie.id)) setWatchList(movie);
+    if (!isPresent)
+      setWatchList([...watchList, movie]);
 
 
     // Check if the movie is already in the watchlist to avoid duplicates
@@ -46,7 +36,14 @@ function Movies() {
     // }
   };
 
+  const removeFromWatchlist = (movie) => {
+    console.log(movie)
+    const filtered = watchList?.filter((ele) => ele.id !== movie.id);
+    setWatchList(filtered)
+  }
+
   console.log(watchList)
+
   useEffect(() => {
     axios
       .get(`https://api.themoviedb.org/3/trending/movie/day?api_key=7af0a3ff53ab2ff0f3f224bb58d5b777&page=${pageNum}`)
@@ -54,28 +51,19 @@ function Movies() {
         setMovies(res.data.results);
       });
   }, [pageNum]);
+
   // console.log(movies)
+
+
+
   return (
     <div className='flex flex-col gap-10 py-12'>
       <h2 className='text-5xl font-bold text-center text-white'>Trending Movies</h2>
       {notification && <p>{notification}</p>}
       <div className='w-4/5 m-auto flex flex-wrap gap-5 justify-center '>
         {movies.map((movie) => (
-          <div
-            key={movie.id}
-            className='w-60 overflow-hidden rounded-xl border-4 hover:scale-105 duration-300 border-gray-800 text-white -inset-600 font-bold text-centre bg-opacity-20'
-          >
-            <div className='absolute cursor-pointer hover:scale-110' onClick={() => addToWatchlist(movie)}>
-              {/* {watchList} */}
-              <img src={bookmark} className='w-[30px] mx-0.5' alt='img' />
-            </div>
-            <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt='img' />
-            <div className='flex gap-2 p-3 text-white'>
-              <img src={star} className='w-[20px] p-l-2' alt='img' />
-              {movie.vote_average.toFixed(1)}
-            </div>
-            <div className='flex p-3 text-white'>{movie.title}</div>
-          </div>
+          <Card movie={movie} addToWatchlist={addToWatchlist} removeFromWatchlist={removeFromWatchlist}/>
+       
         ))}
       </div>
       <Pagination pageNumProp={pageNum} onNextProp={onNext} onPrevProp={onPrev} />
