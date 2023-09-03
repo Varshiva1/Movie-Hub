0109e71c0290b56f60221e1cab76700f31431855
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2023-09-03 21:57:34 +0530

    infinite scolling added

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useRef } from 'react';
 import axios from 'axios';
-import Pagination from './Pagination';
 import WatchList from './WatchList';
 import Card from './Card';
 
@@ -8,16 +7,10 @@ function Movies() {
   const [movies, setMovies] = useState([]);
   const [pageNum, setPageNum] = useState(1);
   const [watchList, setWatchList] = useState([]);
+  const [endOfList, setEndOfList] = useState(false);
+  
 
-  const onNext = () => {
-    setPageNum(pageNum + 1);
-  };
-
-  const onPrev = () => {
-    if (pageNum > 1) {
-      setPageNum(pageNum - 1);
-    }
-  };
+  const movieContainerRef = useRef(null);
 
   const addToWatchlist = (movie) => {
 
@@ -42,10 +35,32 @@ function Movies() {
     axios
       .get(`https://api.themoviedb.org/3/trending/movie/day?api_key=7af0a3ff53ab2ff0f3f224bb58d5b777&page=${pageNum}`)
       .then((res) => {
-        setMovies(res.data.results);
+        const newMovies = res.data.results;
+        if (newMovies.length === 0) {
+          setEndOfList(true);
+        } else {
+          setMovies([...movies, ...newMovies]);
+          setPageNum(pageNum + 1);
+        }
       });
   }, [pageNum]);
 
+  useEffect(() => {
+    function handleScroll() {
+      if (
+        movieContainerRef.current &&
+        window.innerHeight + window.scrollY >= movieContainerRef.current.offsetHeight &&
+        !endOfList
+      ) {
+        // User has scrolled to the bottom, load more movies
+        setPageNum(pageNum + 1);
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [endOfList, pageNum]);
 
   // console.log(movies)
 
@@ -54,13 +69,12 @@ function Movies() {
   return (
     <div className='w-4/5 m-auto flex flex-col gap-10 py-12'>
       <h2 className='text-5xl font-bold text-center text-white'>Trending Movies</h2>
-      <div className=' grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-5'>
+      <div className='grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-5'>
         {movies.map((movie) => (
           <  Card key={movie.id} movie={movie} showBookmark={true}/>
         ))}
       </div>
-      <Pagination pageNumProp={pageNum} onNextProp={onNext} onPrevProp={onPrev} />
-    </div>
+      <WatchList watchList={watchList} removeFromWatchlist={removeFromWatchlist} />    </div>
   );
 }
 
