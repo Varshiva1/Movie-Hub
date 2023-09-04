c292c1770f1094b0140ebfe52d6dc67aac601d1b
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2023-09-04 16:35:34 +0530

    code updated

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom';
 import Card from './Card';
 import { useSelector } from 'react-redux';
 
@@ -8,9 +9,12 @@ function WatchList() {
 
  
  return (// grid-cols-[repeat(auto-fill,minmax(250px,1fr))] used for auto responsive
-  <div className='w-[80%] m-auto py-4 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-x-2 gap-y-4'>{
-  watchList.length > 0 ? watchList.map((movie)=><Card key={movie.id} movie={movie}/>) :""
-  }</div>
+ <>
+  {
+    watchList.length > 0 ? <div className='w-[80%] m-auto py-4 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-x-2 gap-y-4'>{ watchList.map((movie)=> <Card key={movie.id} movie={movie}/>)} </div>:<h1 className='h-screen flex justify-center items-center gap-1 text-white text-2xl text-center'>No movies in watchlist. <Link to="/" className='text-blue-600'> Tap to add</Link></h1>
+    
+  }
+  </>
  )
 }
 
